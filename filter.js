22658aec8f126c8e555ea56c489bf0ccb7f5f184
mathuramg <mmg9291@gmail.com>
2016-09-01 19:22:40 -0400

    change more tags

--- filter.js
+++ filter.js
@@ -1,31 +1,31 @@
 function changeCategory(type){
   //console.log('hi');
   if(type =='programming'){
-    var projects = $('.portfolioHeader').css("display","none");
-    var projects = $('.tag-programming').css("display","inline-block");
+    var projects = $('.projects__item').css("display","none");
+    var projects = $('.tag-programming').css("display","inline-flex");
     //change category to yellow
-    $('.category-tag').removeClass('selected-category');
-    $('.category-tag-programming').addClass('selected-category');
+    $('.nav__category').removeClass('nav__category--selected');
+    $('.nav__category-programming').addClass('nav__category--selected');
   }
   else if(type =='physcomp'){
-    var projects = $('.portfolioHeader').css("display","none");
-    var projects = $('.tag-physcomp').css("display","inline-block");
+    var projects = $('.projects__item').css("display","none");
+    var projects = $('.tag-physcomp').css("display","inline-flex");
     //change category to yellow
-    $('.category-tag').removeClass('selected-category');
-    $('.category-tag-physcomp').addClass('selected-category');
+    $('.nav__category').removeClass('nav__category--selected');
+    $('.nav__category-physcomp').addClass('nav__category--selected');
   }
   else if(type =='animation'){
-    var projects = $('.portfolioHeader').css("display","none");
-    var projects = $('.tag-animation').css("display","inline-block");
+    var projects = $('.projects__item').css("display","none");
+    var projects = $('.tag-animation').css("display","inline-flex");
     //change category to yellow
-    $('.category-tag').removeClass('selected-category');
-    $('.category-tag-animation').addClass('selected-category');
+    $('.nav__category').removeClass('nav__category--selected');
+    $('.nav__category-animation').addClass('nav__category--selected');
   }
   else if(type =='all'){
-    var projects = $('.portfolioHeader').css("display","inline-block");
+    var projects = $('.projects__item').css("display","inline-flex");
     //change category to yellow
-    $('.category-tag').removeClass('selected-category');
-    $('.category-tag-all').addClass('selected-category');
+    $('.nav__category').removeClass('nav__category--selected');
+    $('.nav__category-all').addClass('nav__category--selected');
   }
 }
 function load(){
